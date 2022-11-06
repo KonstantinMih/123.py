0d3d83142fb64d334570ef262561f94c323c1e7c
ko5tik <mihkostik@gmail.com>
2022-11-06 19:23:43 +0300

    Сделано всё, кроме таблицы игроков

--- lab/111.py
+++ lab/111.py
@@ -1,10 +1,17 @@
 import pygame
 from pygame.draw import *
 from random import randint
-pygame.init()
+import socket
+pygame.font.init()
 
-FPS = 2
+FPS = 60
 screen = pygame.display.set_mode((1200, 900))
+
+name = socket.gethostname()
+records = open('Таблица игроков.txt', 'w')
+
+score = 0
+balls = []
 
 RED = (255, 0, 0)
 BLUE = (0, 0, 255)
@@ -15,13 +22,41 @@ CYAN = (0, 255, 255)
 BLACK = (0, 0, 0)
 COLORS = [RED, BLUE, YELLOW, GREEN, MAGENTA, CYAN]
 
-def new_ball():
-    '''рисует новый шарик '''
-    x = randint(100, 1100)
-    y = randint(100, 900)
-    r = randint(10, 100)
-    color = COLORS[randint(0, 5)]
-    circle(screen, color, (x, y), r)
+def new_balls():
+    '''генерирует новые шарики '''
+    if len(balls) < 4:
+        x = randint(100, 700)
+        y = randint(100, 500)
+        r = randint(50, 70)
+        color = COLORS[randint(0, 5)]
+        speed_x = randint(-15, 15)
+        speed_y = randint(-15, 15)
+        balls.append([color, (x, y), r, speed_x, speed_y])
+
+def draw_balls(screen, balls):
+    '''Отрисовывет шар
+    screen - экран
+    balls - список сгенерированных шаров'''
+    for b in balls:
+        circle(screen, b[0], b[1], b[2])
+
+def move_balls():
+    '''двигает шары'''
+    for b in balls:
+        if b[1][1] - b[2] <= 0 or b[1][1] + b[2] >= 900:
+            b[4] = -b[4]
+        if b[1][0] - b[2] <= 0 or b[1][0] + b[2] >= 1200:
+            b[3] = -b[3]
+        x = b[1][0] + b[3]
+        y = b[1][1] + b[4]
+        b[1] = (x, y)
+
+def click(event):
+    ''' засекает, попали ли кликом в шарик, и удаляет шар, в который попали кликом, начисляя очки '''
+    for b in balls:
+        if ((event.pos[0] - b[1][0])**2 + (event.pos[1] - b[1][1])**2) <= b[2]**2:
+            balls.remove(b)
+    return 4 - len(balls)
 
 pygame.display.update()
 clock = pygame.time.Clock()
@@ -32,9 +67,17 @@ while not finished:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             finished = True
+            space = 20*' '
+            records.write(name + space + str(score))
+            records.close()
         elif event.type == pygame.MOUSEBUTTONDOWN:
-            print('Click!')
-    new_ball()
+            if click(event) != 0:
+                score = score + click(event)
+                print(score)
+                print(name)
+    new_balls()
+    draw_balls(screen, balls)
+    move_balls()
     pygame.display.update()
     screen.fill(BLACK)
 
